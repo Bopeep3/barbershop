61ffdca1f0fd9c723b359d97c785977d24332661
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2024-04-29 22:54:59 +0530

    first commit

---
+++ web-portal/frontend/src/serviceData.js
@@ -0,0 +1,8 @@
+// src/serviceData.js
+
+export const services = [
+    { value: 'beard-trim', label: 'Beard trim' },
+    { value: 'hair-cut', label: 'Hair cut' },
+    { value: 'massage', label: 'Massage' },
+    { value: 'perm', label: 'Perm' },
+];
